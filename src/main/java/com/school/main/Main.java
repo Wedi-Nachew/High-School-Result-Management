86c15b69270e4e163830dcd09ff99248fd77ec8b
Wedi-Nachew <131978339+Wedi-Nachew@users.noreply.github.com>
2025-02-09 16:38:23 +0300

    Create the Main class

---
+++ src/main/java/com/school/main/Main.java
@@ -0,0 +1,12 @@
+package com.school.main;
+
+import com.school.ui.MenuSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuSystem menu = new MenuSystem();
+        menu.displayMenu();
+    }
+}
+
+
